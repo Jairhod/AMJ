83aaadc2402240a04b414e1cef0209e755ad4403
Jairhod <benjair@gmail.com>
2017-03-02 21:59:41 +0100

    Ajax pour la recherche par genre

--- private/view/section-accueil-recherche.php
+++ private/view/section-accueil-recherche.php
@@ -6,12 +6,15 @@
             <input type="hidden" name="idForm" value="choixAccueil">
             <div class="bla">
                
-                <select name="genres" id="genres" class="form-control form-accueil">                    
-                    <option value="2">Pop</option>
-                    <option value="3">Soul-Funk-Blues</option>
+                <select name="genres" id="genres" class="form-control form-accueil">
+                    <option value="0" selected disabled>Choisir son style...</option>                 
+                    <option value="SOUL-FUNK-BLUES">SOUL - FUNK - R &amp; BLUES</option>
+                    <option value="POP-ROCK-ACTU">POP - ROCK - ACTU</option>
+                    <option value="tous">Tous styles</option>
                 </select>
               
-                <select name="artistes" id="artistes" class="form-control form-accueil form-artistes-accueil">               
+                <select name="artistes" id="artistes" class="form-control form-accueil form-artistes-accueil">
+                    <option value="0" selected disabled>Choisir son artiste...</option>             
                 </select> 
                              
                 <button type="submit" name="envoyerAccueil" class="search-submit-accueil"><img src="assets/css/img/search.png" alt=""></button>
@@ -25,7 +28,7 @@
         $(document).ready(function(){
             $('#genres').on('change', function(){
                //alert($(this).val());
-                $.get('http://localhost/myphp/github/amj/private/controler/ajax/recupGenres.php', {artiste: $(this).val()}, function(data){
+                $.get('private/controler/ajax/recupGenre.php', {genre: $(this).val()}, function(data){
                    $('#artistes').html(data);
                 });
             });
