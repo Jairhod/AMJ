1d15dffe08d13ab224a5e7977e19772e1138444d
Jonathan <perezjonathancdp@gmail.com>
2017-03-02 16:53:42 +0100

    clean script
    clean  script & link

--- script/script.js
+++ script/script.js
@@ -3,10 +3,3 @@ console.log('Javascript is linked & jQuery is installed :)')
 /* Script Prallax Accueil ========== */
 
 
-/*$(document).ready(function(){
-      $('#recherche-accueil').parallax();
-    });*/
-
-$('.bg-holder').parallaxScroll({
-  friction: 0.5
-});
