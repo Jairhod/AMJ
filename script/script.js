2951f01a6c4f571ba65e451fd24331a2a7d6849f
Jonathan <perezjonathancdp@gmail.com>
2017-03-06 16:50:58 +0100

    vignettes de genre sur la section recherche (test)
    test css sur vignettes

--- script/script.js
+++ script/script.js
@@ -2,16 +2,3 @@ console.log('Javascript is linked & jQuery is installed :)')
 
 /* Script Prallax Accueil ========== */
 
-$(document).ready(function() {
-  var origheight = $("#trans1").height();
-  var height = $(window).height();
-  if (height > origheight) {
-    $("#trans1").height(height);
-  }
-  
-  $(window).scroll(function(){
-    var x = $(this).scrollTop();
-    $('#trans1').css('background-position','center -'+parseInt(x/5)+'px');
-});
-  
-});
