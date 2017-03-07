5c43ab467a7caf0106a26ea39b4fc13affd393ad
Jonathan <perezjonathancdp@gmail.com>
2017-03-07 14:52:41 +0100

    responsive Home Page
    footer, contact, label

--- private/view/section-accueil-descriptif.php
+++ private/view/section-accueil-descriptif.php
@@ -4,15 +4,15 @@
             <h3>Descriptif</h3>
         </div>
         <div class="descriptif-row">
-            <div class="col-md-4">
+            <div class="col-md-4 descript-style">
                 <div class="descriptif-img"><img src="assets/css/img/drum-set.png" alt="" class="descriptif-img"></div>
                 <p>Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum, qui permet donc de faire office de texte d'attente. L'avantage de le mettre en latin est que l'opérateur sait au premier coup d'oeil que la page contenant ces lignes n'est pas valide, et surtout l'attention du client n'est pas dérangée par le contenu, il demeure concentré seulement sur l'aspect graphique.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 descript-style">
                 <div class="descriptif-img"><img src="assets/css/img/levels.png" alt="" ></div>
                 <p>Ce texte a pour autre avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale. La méthode simpliste consistant à copier-coller un court texte plusieurs fois (« ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ») a l'inconvénient de ne pas permettre une juste appréciation typographique du résultat final.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 descript-style">
                 <div class="descriptif-img"><img src="assets/css/img/question-class-note-symbol.png" alt="" ></div>
                 <p>Ce texte a pour autre avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale. La méthode simpliste consistant à copier-coller un court texte plusieurs fois (« ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ») a l'inconvénient de ne pas permettre une juste appréciation typographique du résultat final.</p>
             </div>
