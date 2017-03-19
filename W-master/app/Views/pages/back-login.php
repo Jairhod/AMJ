79aef47f29c6a4295f4b512d25b3762dff4d54c3
Jairhod <benjair@gmail.com>
2017-03-19 22:49:12 +0100

    migration

--- W-master/app/Views/pages/back-login.php
+++ W-master/app/Views/pages/back-login.php
@@ -1,4 +1,4 @@
 <?php
-$this->insert('partials/back-header', [ "titre" => "Login du back office" ]);
+$this->insert('partials/back-header', [ "titre" => "Login du back-office" ]);
 $this->insert('partials/back-section-login');
 $this->insert('partials/back-footer');
--- W-master/app/Views/pages/fiche-artiste.php
+++ W-master/app/Views/pages/fiche-artiste.php
@@ -2,8 +2,6 @@
 $this->insert('partials/header', [ "titre" => "Fiche artiste" ]);
 $this->insert('partials/nav');
 $this->insert('partials/section-catalogue-nav');
-
 $this->insert('partials/section-artiste');
 $this->insert('partials/section-artiste-lies');
-
 $this->insert('partials/footer');
