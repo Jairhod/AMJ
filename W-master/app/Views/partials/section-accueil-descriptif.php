0cd347bf2bb2ad9b379d42dd7203d97c85caa873
Jonathan <perezjonathancdp@gmail.com>
2017-03-22 08:09:36 +0100

    css HP
    section descriptif: contenu et css

--- W-master/app/Views/partials/section-accueil-descriptif.php
+++ W-master/app/Views/partials/section-accueil-descriptif.php
@@ -1,20 +1,24 @@
 <div class="container-fluid full-width">
     <section id="descriptif">
         <div class="h3-des">
-            <h3>Descriptif</h3>
+            <h3>AMJ, comment ça fonctionne ?</h3>
         </div>
         <div class="descriptif-row">
-            <div class="col-md-4 descript-style">
-                <div class="descriptif-img"><img src="assets/media/img/drum-set.png" alt="" class="descriptif-img"></div>
-                <p>Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum, qui permet donc de faire office de texte d'attente. L'avantage de le mettre en latin est que l'opérateur sait au premier coup d'oeil que la page contenant ces lignes n'est pas valide, et surtout l'attention du client n'est pas dérangée par le contenu, il demeure concentré seulement sur l'aspect graphique.</p>
+            <div class="col-md-3 descript-style">
+                <div class="descriptif-img"><img src="assets/media/img/recherche-group.png" alt="" class="descriptif-img"></div>
+                <p>1.  Vous êtes un particulier, une société, une institution publique et vous cherchez un groupe pour un évènement public ou privé (festival, concert, mariage, congrès…)? </p>
             </div>
-            <div class="col-md-4 descript-style">
-                <div class="descriptif-img"><img src="assets/media/img/levels.png" alt=""></div>
-                <p>Ce texte a pour autre avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale. La méthode simpliste consistant à copier-coller un court texte plusieurs fois (« ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ») a l'inconvénient de ne pas permettre une juste appréciation typographique du résultat final.</p>
+            <div class="col-md-3 descript-style">
+                <div class="descriptif-img"><img src="assets/media/img/list-with-possible-workers-to-choose.png" alt=""></div>
+                <p>2.  Consultez notre catalogue d’artistes, recherchez, triez et trouvez selon vos envies !</p>
             </div>
-            <div class="col-md-4 descript-style">
-                <div class="descriptif-img"><img src="assets/media/img/question-class-note-symbol.png" alt=""></div>
-                <p>Ce texte a pour autre avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale. La méthode simpliste consistant à copier-coller un court texte plusieurs fois (« ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ») a l'inconvénient de ne pas permettre une juste appréciation typographique du résultat final.</p>
+            <div class="col-md-3 descript-style">
+                <div class="descriptif-img"><img src="assets/media/img/approuve.png" alt=""></div>
+                <p>3.	Simulez un devis personnalisé ! Prenez contact avec notre équipe pour finaliser votre demande !</p>
+            </div>
+            <div class="col-md-3 descript-style">
+                <div class="descriptif-img"><img src="assets/media/img/heavy-metal.png" alt=""></div>
+                <p>4.	Réservez le groupe que vous avez choisi !</p>
             </div>
         </div>
     </section>
