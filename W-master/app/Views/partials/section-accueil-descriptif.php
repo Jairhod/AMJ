3334bf94490cca95e3be747b60d2bc87bd030973
Jonathan <perezjonathancdp@gmail.com>
2017-03-22 17:28:44 +0100

    css hoME pAGE
    SECTION DESCRIPTION RESPONSIVE

--- W-master/app/Views/partials/section-accueil-descriptif.php
+++ W-master/app/Views/partials/section-accueil-descriptif.php
@@ -4,21 +4,21 @@
             <h3>AMJ, comment ça fonctionne ?</h3>
         </div>
         <div class="descriptif-row">
-            <div class="col-md-3 descript-style">
+            <div class="col descript-style">
                 <div class="descriptif-img"><img src="assets/media/img/recherche-group.png" alt="" class="descriptif-img"></div>
-                <p>1.  Vous êtes un particulier, une société, une institution publique et vous cherchez un groupe pour un évènement public ou privé (festival, concert, mariage, congrès…)? </p>
+                <p>1.  <strong>Vous êtes un particulier</strong>, une société, une institution publique et <strong>vous cherchez un groupe</strong> pour un évènement public ou privé (festival, concert, mariage, congrès…)? </p>
             </div>
-            <div class="col-md-3 descript-style">
+            <div class="col descript-style">
                 <div class="descriptif-img"><img src="assets/media/img/list-with-possible-workers-to-choose.png" alt=""></div>
-                <p>2.  Consultez notre catalogue d’artistes, recherchez, triez et trouvez selon vos envies !</p>
+                <p>2.  <strong>Consultez notre catalogue d’artistes</strong>, recherchez, triez et trouvez selon vos envies !</p>
             </div>
-            <div class="col-md-3 descript-style">
+            <div class="col descript-style">
                 <div class="descriptif-img"><img src="assets/media/img/approuve.png" alt=""></div>
-                <p>3.	Simulez un devis personnalisé ! Prenez contact avec notre équipe pour finaliser votre demande !</p>
+                <p>3.	<strong>Simulez un devis personnalisé</strong> ! Prenez contact avec notre équipe pour finaliser votre demande !</p>
             </div>
-            <div class="col-md-3 descript-style">
+            <div class="col descript-style">
                 <div class="descriptif-img"><img src="assets/media/img/heavy-metal.png" alt=""></div>
-                <p>4.	Réservez le groupe que vous avez choisi !</p>
+                <p>4.	<strong>Réservez</strong> le groupe que vous avez choisi !</p>
             </div>
         </div>
     </section>
