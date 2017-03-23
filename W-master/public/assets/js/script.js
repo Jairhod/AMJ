bc97faba75f66a91b3ef50941bfdebf9b93a4ee6
Jonathan <perezjonathancdp@gmail.com>
2017-03-23 16:09:44 +0100

    css Home Page + responsive et page actus
    css Home Page + responsive et page actus

--- W-master/public/assets/js/script.js
+++ W-master/public/assets/js/script.js
@@ -1,4 +1,31 @@
 console.log('Javascript is linked & jQuery is installed :)')
 
-/* Script Prallax Accueil ========== */
+
+$(document).ready(function(){
+    
+/* Effet au scroll scrollreveal ========== */
+   
+    window.sr = ScrollReveal({reset: true});
+    var fooReveal = {
+        distance : '50px',
+        easing   : 'ease-in-out',
+        scale    : 1.1
+    };
+    
+    sr.reveal('.anim-descr', fooReveal);
+
+    
+/* API FACEBOOK : pour fil d'actu */
+
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8&appId=1417053915025497";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    
+});
+    
 
