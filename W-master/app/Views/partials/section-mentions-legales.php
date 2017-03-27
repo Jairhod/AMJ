52b76185088f4815f33903bffae7039aef288b5b
Jonathan <perezjonathancdp@gmail.com>
2017-03-27 14:01:15 +0200

    mentions legales end
    mentions legales end

--- W-master/app/Views/partials/section-mentions-legales.php
+++ W-master/app/Views/partials/section-mentions-legales.php
@@ -1,6 +1,6 @@
         <section id="mentions-legales">
             <div class="mentions-wrapper">
-                <h1>Mentions légales</h1>
+                <h1>Mentions légales</h1><br>
                 <h2>Informations légales</h2>
                 <h3>1. Présentation du site.</h3>
                     <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="http://amjprod.com/">amjprod.com</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
