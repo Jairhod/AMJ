3e31c55c18a1144ef0eb8bf4f3178ab620f0ece2
Jonathan <perezjonathancdp@gmail.com>
2017-03-27 15:09:01 +0200

    crea Namespace et classe eventviva
    pour resize photos pour vignttes de la page catalogue

--- W-master/app/Views/partials/section-label.php
+++ W-master/app/Views/partials/section-label.php
@@ -2,21 +2,21 @@
             <div class="label-wrapper">
                 <h1>AMJ: Le label</h1>
                 <div class="intro">
-                    <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, le collectif JauneOrange multiplie les casquettes, démontrant le dynamisme sans faille de la ville de Liège dans la sphère de l’indie rock. Son leitmotiv : une démarche résolument DIY et un esprit fraternel salvateur. Rencontre avec Maxime Lhussier, salarié du collectif et musicien dans plusieurs des groupes jauneorangés (Dan San, Pale Grey et The Feather).
+                    <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, AMJ multiplie les casquettes, démontrant le dynamisme sans faille de la vrégion Provence-Alpes-Cote-D'Azur dans la sphère musicale éclectique. Son leitmotiv : une démarche résolument positive et un esprit fraternel salvateur. 
                     </p>
                 </div>
                 <div class="col">
-                    <h2>COMMENT EST NÉ JAUNEORANGE ?</h2>
-                    <p>En 2000, plusieurs groupes se croisaient régulièrement sur les petites scènes de Liège. Ils ont décidé de se fédérer, selon un principe simple : l’union fait la force. A partir de là, ils ont créé un site Internet – Myspace n’existait même pas, à l’époque… Petit à petit, les activités se sont développées. Le collectif a commencé à organiser des concerts dans des petites salles avec des artistes étrangers “indie” et des premières parties assurées par des groupes du collectif. Les choses ont continué à se développer. Depuis neuf ou dix ans, on a une agence de booking et une section label et, depuis cinq ans, notre festival d’été, le Micro Festival.</p>
+                    <h2>COMMENT EST NÉ AMJ ?</h2>
+                    <p>En 2000, plusieurs groupes se croisaient régulièrement sur les petites scènes de La région. Ils ont décidé de se fédérer, selon un principe simple : l’union fait la force. A partir de là, ils ont créé un site Internet – Myspace n’existait même pas, à l’époque… Petit à petit, les activités se sont développées. Le collectif a commencé à organiser des concerts dans des petites salles avec des artistes étrangers “indie” et des premières parties assurées par des groupes du collectif. Les choses ont continué à se développer. Depuis neuf ou dix ans, on a une agence de booking et une section label et, depuis cinq ans, notre festival d’été, le Micro Festival.</p>
                 </div>
 
                 <div class="col">
                     <h2>VOUS N’ÉTIEZ VOUS-MÊME PAS PRÉSENT À LA CRÉATION DU COLLECTIF ?</h2>
-                    <p>Non, j’ai rejoint JauneOrange il y a quatre ans mais, quand j’étais plus jeune, j’allais beaucoup aux concerts et j’étais déjà fan du collectif, notamment d’artistes comme Hollywood Porn Stars ou Malibu Stacy. A la base, je suis musicien dans le groupe Dan San, signé sur le label. Quand j’ai terminé mes études, ils m’ont proposé de rejoindre les rangs de l’équipe à mi-temps.</p>
+                    <p>Non, j’ai rejoint AMJ il y a quatre ans mais, quand j’étais plus jeune, j’allais beaucoup aux concerts et j’étais déjà fan du collectif, notamment d’artistes comme Hollywood Porn Stars ou Malibu Stacy. A la base, je suis musicien dans le groupe Dan San, signé sur le label. Quand j’ai terminé mes études, ils m’ont proposé de rejoindre les rangs de l’équipe à mi-temps.</p>
                 </div>
 
                 <div class="col">
-                    <h2>ON SENT UNE SORTE D’ESPRIT FAMILIAL CHEZ JAUNEORANGE…</h2>
+                    <h2>ON SENT UNE SORTE D’ESPRIT FAMILIAL CHEZ AMJ…</h2>
                     <p>Oui, les groupes se côtoient beaucoup de manière naturelle. On répète plus ou moins dans les mêmes endroits et, comme on organise des concerts, ça arrive souvent qu’il y ait dans le public des musiciens du collectif. Il y a aussi beaucoup de “relations incestueuses” entre les différents groupes. On retrouve parfois des groupes confirmés du label à la production de disques de groupes plus jeunes. Les musiciens se donnent des conseils, se prêtent du matériel. On a un système de location de van, pour les artistes. On est un peu impliqués à tous les niveaux.</p>
                 </div>
 
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="col">
-                    <h2>AU-DELÀ DU COLLECTIF JAUNEORANGE, COMMENT PERCEVEZ-VOUS LA SCÈNE MUSICALE EN REGION PACA ?</h2>
+                    <h2>AU-DELÀ DU COLLECTIF AMJ, COMMENT PERCEVEZ-VOUS LA SCÈNE MUSICALE EN REGION PACA ?</h2>
                     <p>Nous sommes un petit pays, qui n’a pas une tradition culturelle très forte, ni très impressionnante, à l’inverse de la France. Chez vous, il peut s’avérer difficile de se situer, avec toutes les choses qui ont existé précédemment, par exemple le riche héritage de la chanson française. En Belgique, on a beaucoup moins de pression, un peu comme si personne ne nous attendait. On fait donc peut-être les choses de manière plus décomplexée. Avec beaucoup de passion, mais sans trop se poser de questions, en attendant de voir ce que cela donne. Et c’est souvent dans ces cas-là que les plus belles choses se passent...</p>
                 </div>
             </div>
