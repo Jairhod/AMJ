86526088b35a5570db4dd7cd53604a361e132cc0
domilibanio <domilibanio@yahoo.fr>
2017-03-28 17:03:01 +0200

    menu fiche artistes

--- W-master/app/Views/partials/section-artiste.php
+++ W-master/app/Views/partials/section-artiste.php
@@ -1,33 +1,48 @@
-      <link rel="stylesheet" type="text/css" href="assets/css/style-do.css">
-      <div id="breadcrumbs"><a href="#!">artiste machin</a></div>
-      
-      <div id="recherche-artiste">
-        <form action="" method="GET"></form>
-        <input type="hidden" name="idForm" value="rechercheArtiste">
-        <button type="submit" name="rechercher" value="rechercher">Rechercher un artiste</button>
+  
+<!--menu style musical -->  
+    <div class="container demo-1">  
+      <div class="main clearfix">
+        <div class="column">
+          <div id="dl-menu" class="dl-menuwrapper">
+            <button class="dl-trigger">Open Menu</button>
+            <div class="spangenres">
+              <span>Style musical</span>
+            </div>
+            <ul class="dl-menu">
+              <li>
+                <a href="#">Jazz</a>
+              </li>
+              <li>
+                <a href="#">Soul Funk</a>
+              </li>
+              <li>
+                <a href="#">Orchestre</a>
+              </li>
+              <li>
+                <a href="#">R' &amp Blues</a>
+              </li>
+              <li>
+                <a href="#">Dance Floor</a>
+              </li>
+              <li>
+                <a href="#">Duo, Trio, Quartet</a>
+              </li>
+              <li>
+                <a href="#">Strolling</a>
+              </li>
+              <li>
+                <a href="#">Festivals</a>
+              </li>
+              <li>
+                <a href="#">Tribute</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <section>
-      		
-            <!--NAV BAR STYLE MUSICAL-->
-           
+    </div>
+<!--photo principaleartiste-->    
 
-           <nav class="navbar navbar-default menustyles">
-              <div class="container-fluid">
-                <div class="navbar-collapse">
-                    <ul>
-                      <li><a href="#">Jazz</a></li>
-                      <li><a href="#">Rock</a></li>
-                      <li><a href="#">R' & Blues</a></li>
-                      <li><a href="#">Soul/Funk</a></li>
-                      <li><a href="#">World Music</a></li>
-                      <li><a href="#">Tribute</a></li>
-                      <li><a href="#">Duo, Trio...</a></li>
-                      <li><a href="#">Festivals</a></li>
-                      <li><a href="#">Dance Floor</a></li>
-                      <li><a href="#">Orchestre</a></li>
-                      <li><a href="#">Strolling</a></li>
-                      <li><a href="#">Magie</a></li>
-                    </ul>
-              </div>
-             </div>  
-          </nav>
+
+    
+ 
