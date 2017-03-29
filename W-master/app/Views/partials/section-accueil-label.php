2653450e98b075f80f02390dfd4eb2579da9b578
Jonathan <perezjonathancdp@gmail.com>
2017-03-29 13:22:01 +0200

    ajout date dynamique footer, header style css, ets ection label et catalogue
    h

--- W-master/app/Views/partials/section-accueil-label.php
+++ W-master/app/Views/partials/section-accueil-label.php
@@ -1,9 +1,15 @@
 	<div class="container-fluid">
-		<section id="label-accueil"></section>
-		<h3>Le Label</h3>
-		<div><img src="" alt="le label"></div>
-		<article>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis commodi quod quos, nam corrupti. Iste excepturi soluta iusto quis consequuntur, saepe deserunt, sapiente, nostrum nesciunt necessitatibus quasi rerum eligendi ex! </p>
-		</article>
-		<a href="">Lire la suite...</a>
+		<section id="label-accueil">
+            <div class="label-accueil-wrapper">
+                <h3>Le label</h3>
+                <div><img src="" alt="le label"></div>
+                <article class="col">
+                    <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, AMJ multiplie les casquettes, démontrant le dynamisme sans faille de la vrégion Provence-Alpes-Cote-D'Azur dans la sphère musicale éclectique. Son leitmotiv : une démarche résolument positive et un esprit fraternel salvateur.</p>
+                    <a href="">Lire la suite...</a>
+                </article>
+                <div class="col">
+                    
+                </div>                
+            </div>
+        </section>
 	</div>
