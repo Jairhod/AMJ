235d6ae40048b475c90e372c27cb40763705ff0e
Jonathan <perezjonathancdp@gmail.com>
2017-03-30 12:00:31 +0200

    label section + photos

--- W-master/app/Views/partials/section-accueil-label.php
+++ W-master/app/Views/partials/section-accueil-label.php
@@ -1,15 +1,17 @@
-	<div class="container-fluid">
-		<section id="label-accueil">
-            <div class="label-accueil-wrapper">
-                <h3>Le label</h3>
-                <div><img src="" alt="le label"></div>
-                <article class="col">
-                    <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, AMJ multiplie les casquettes, démontrant le dynamisme sans faille de la vrégion Provence-Alpes-Cote-D'Azur dans la sphère musicale éclectique. Son leitmotiv : une démarche résolument positive et un esprit fraternel salvateur.</p>
-                    <a href="">Lire la suite...</a>
-                </article>
-                <div class="col">
-                    
-                </div>                
+            <div class="container-fluid">
+                <section id="label-accueil">
+                    <h3>Le label</h3>
+                    <div class="label-accueil-wrapper">
+                       <!-- <div class="label-img"><img src="assets/media/img/studio-1.jpg" alt="photo du label" class="img-responsive"></div>-->
+                        <article class="col-label">
+                            <div class="label-descriptif">
+                                <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, AMJ multiplie les casquettes, démontrant le dynamisme sans faille de la vrégion Provence-Alpes-Cote-D'Azur dans la sphère musicale éclectique. Son leitmotiv : une démarche résolument positive et un esprit fraternel salvateur.</p>
+                                <a href="">Lire la suite...</a>
+                            </div>
+                        </article>
+                        <div class="col-label">
+
+                        </div>
+                    </div>
+                </section>
             </div>
-        </section>
-	</div>
