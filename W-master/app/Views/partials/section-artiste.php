1ced610d1b71f289c1ccf9ce94879cfb35733f25
domilibanio <domilibanio@yahoo.fr>
2017-03-31 16:52:54 +0200

    fiche artiste
    # Conflicts:
    #	BDD/amj_bdd.sql

--- W-master/app/Views/partials/section-artiste.php
+++ W-master/app/Views/partials/section-artiste.php
@@ -2,167 +2,125 @@
 
 <!--PHOTO ET TEXTE PRESENTATION-->  
 <div class="container containerblue">
-<div class="borderbleuvert">
-  <div id="photoprincipale">
-      <center><img src="./assets/media/img/3/images/Quartet-Soul-3.jpg" width="70%" alt="blackout band"></center>
-      <div class="h2centre">
-      <h2>BLACKOUT BAND</h2>
-      </div>
-  </div>
-  </div>
-    <div class="row">   
-        <div class=" col-md-6 texteficheartiste">
-          <p>C’est sur les scènes et clubs du sud de la France que les quatre membres du groupe BACKOUT se sont rencontrés <br />afin de rendre hommage au plus grandes voix féminines de la SOUL et du R&B.
-          </p>
-          <p>
-          Emile MÉLENCHON (guitare et arrangements), Arnaud PACINI (basse) et Marc BELLION (batterie), Andréa CAPARROS (Piano et choeurs)<br /> venant tout juste d’intégrer la formation, accompagnent Nicole LISE (chant), incarnant à elle seule <br />toutes les qualités nécessaires à l’interprétation de ce répertoire pour nous faire voyager dans le temps.
-          </p>
-          <p>
-          D’ARETHA FRANKLIN à BEYONCE en passant par SHAKA KHAN, ERYKA BADU et bien d’autres, <br />le BLACKOUT Band vous fera partager leur énergie communicative.
-          </p>  
+  <div class="borderbleuvert">
+    <div id="photoprincipale">
+        <center><img src="./assets/media/img/3/images/Quartet-Soul-3.jpg" width="70%" alt="blackout band"></center>
+        <div class="h2centre">
+        <h2>BLACKOUT BAND</h2>
         </div>
-      <!--/////////////////code poour player audio//////////////////////////////////////////--> 
+    </div>
+  </div>
+      <div class="row">   
+          <div class=" col-md-6 texteficheartiste">
+            <p>C’est sur les scènes et clubs du sud de la France que les quatre membres du groupe BACKOUT se sont rencontrés <br />afin de rendre hommage au plus grandes voix féminines de la SOUL et du R&B.
+            </p>
+            <p>
+            Emile MÉLENCHON (guitare et arrangements), Arnaud PACINI (basse) et Marc BELLION (batterie), Andréa CAPARROS (Piano et choeurs)<br /> venant tout juste d’intégrer la formation, accompagnent Nicole LISE (chant), incarnant à elle seule <br />toutes les qualités nécessaires à l’interprétation de ce répertoire pour nous faire voyager dans le temps.
+            </p>
+            <p>
+            D’ARETHA FRANKLIN à BEYONCE en passant par SHAKA KHAN, ERYKA BADU et bien d’autres, <br />le BLACKOUT Band vous fera partager leur énergie communicative.
+            </p>  
+          </div>
+        <!--/////////////////code poour player audio//////////////////////////////////////////--> 
+        
+        <div class="col-md-6">  
+          <section class="audiosection">
+            <div class="containeraudio">
+  <!--            <div class="columnaudio center"></div>-->
+              <div class="column add-bottom">
+                  <div id="mainwrap">
+                      <div id="nowPlay">
+                          <span class="left" id="npAction">Paused...</span>
+                          <span class="right" id="npTitle"></span>
+                      </div>
+                      <div id="audiowrap">
+                          <div id="audio0">
+                              <audio preload id="audio1" controls="controls">Your browser does not support HTML5 Audio!</audio>
+                          </div>
+                          <div id="tracks">
+                              <a id="btnPrev">&laquo;</a>
+                              <a id="btnNext">&raquo;</a>
+                          </div>
+                      </div>
+                      <div id="plwrap">
+                          <ul id="plList"></ul>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>  
       
-      <div class="col-md-6">  
-        <section class="audiosection">
-          <div class="containeraudio">
-<!--            <div class="columnaudio center"></div>-->
-            <div class="column add-bottom">
-                <div id="mainwrap">
-                    <div id="nowPlay">
-                        <span class="left" id="npAction">Paused...</span>
-                        <span class="right" id="npTitle"></span>
-                    </div>
-                    <div id="audiowrap">
-                        <div id="audio0">
-                            <audio preload id="audio1" controls="controls">Your browser does not support HTML5 Audio!</audio>
-                        </div>
-                        <div id="tracks">
-                            <a id="btnPrev">&laquo;</a>
-                            <a id="btnNext">&raquo;</a>
-                        </div>
-                    </div>
-                    <div id="plwrap">
-                        <ul id="plList"></ul>
-                    </div>
-                </div>
-            </div>
+   <!--CAROUSEL -->
+   <div class="row">
+    <div class="backgroundcarousel">
+      <div class="bs-example">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <!-- Carousel indicators -->
+              <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+              </ol>   
+              <!-- Wrapper for carousel items -->
+              <div class="carousel-inner">
+                  <div class="item active">
+                      <img src="./assets/media/img/3/images/Soul-Quartet-Marc-1.jpeg" alt="First Slide">
+                  </div>
+                  <div class="item">
+                      <img src="./assets/media/img/3/images/Soul-Quartet-Nicole-1.jpeg" alt="Second Slide">
+                  </div>
+                  <div class="item">
+                      <img src="./assets/media/img/3/images/Soul-Quartet-6.jpeg" alt="Third Slide">
+                  </div>
+              </div>
+              <!-- Carousel controls -->
+              <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+              </a>
+              <a class="carousel-control right" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+              </a>
           </div>
-        </section>
       </div>
-    </div>  
-    
- <!--CAROUSEL -->
+    </div> 
+  </div>   
 
-    <div class="row">
-      <div class="col-md-12">   
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="./assets/media/img/3/images/Soul-Quartet-Marc-1.jpeg" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="./assets/media/img/3/images/Soul-Quartet-Nicole-1.jpeg" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="./assets/media/img/3/images/Soul-Quartet-6.jpeg" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>   
-    </div>  
-
-  <!--CAROUSEL -->
-<!--  <div class="borderbleuvert">
+   
+  <!--VIDEO--> 
   <div class="row">
-    <div class="col-md-12">
-        <div class="carousel">
-          <div class="holder">
-            <img src="./assets/media/img/3/images/Quartet-Soul-3.jpg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-1.jpg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-6.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Andrea-2.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Andrea-4.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Arnaud-1.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Marc-1.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Nicole-1.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Nicole-7.jpeg" alt="quartet-soul" />
-            <img src="./assets/media/img/3/images/Soul-Quartet-Nicole-8.jpeg" alt="quartet-soul" />
-          </div>
-        </div>
-    </div>
-  </div>
-  </div>
--->
-<!--VIDEO--> 
-
-  <div class="row borderbleuvert">
- 
-    <div class="col-md-12">
-      <div class="centre">
-          
-          <iframe width="334" height="229" src="https://www.youtube.com/embed/Fz3DDoNq5Wk" frameborder="0" allowfullscreen class="marginvideo"></iframe>
-          <iframe width="335" height="229" src="https://www.youtube.com/embed/Ffo4ZxWfEGs" frameborder="0" allowfullscreen class="marginvideo"></iframe>
-          <iframe width="334" height="229" src="https://www.youtube.com/embed/PPgKnxSXGms" frameborder="0" allowfullscreen class="marginvideo"></iframe>
-    </div>      
-    </div>  
-    </div>    
-    
- <!--AUDIO MEDIAPLAYER-->  
-
-
- <div class="row borderbleuvert">
+    <div class="backgroundvideo">
    
-
-<!--/////////////////code poour player audio//////////////////////////////////////////   
-<section class="audiosection">
-<div class="containeraudio">
-    <div class="columnaudio center">
+      <div class="col-md-12 backgroundvideo">
+        <div class="centre">
+            <iframe width="334" height="229" src="https://www.youtube.com/embed/Fz3DDoNq5Wk" frameborder="0" allowfullscreen class="marginvideo"></iframe>
+            <iframe width="335" height="229" src="https://www.youtube.com/embed/Ffo4ZxWfEGs" frameborder="0" allowfullscreen class="marginvideo"></iframe>
+            <iframe width="334" height="229" src="https://www.youtube.com/embed/PPgKnxSXGms" frameborder="0" allowfullscreen class="marginvideo"></iframe>
+        </div>      
+      </div>  
     </div>
-    <div class="column add-bottom">
-        <div id="mainwrap">
-            <div id="nowPlay">
-                <span class="left" id="npAction">Paused...</span>
-                <span class="right" id="npTitle"></span>
-            </div>
-            <div id="audiowrap">
-                <div id="audio0">
-                    <audio preload id="audio1" controls="controls">Your browser does not support HTML5 Audio!</audio>
-                </div>
-                <div id="tracks">
-                    <a id="btnPrev">&laquo;</a>
-                    <a id="btnNext">&raquo;</a>
-                </div>
-            </div>
-            <div id="plwrap">
-                <ul id="plList"></ul>
-            </div>
+  </div>  
+    <div class="row">
+        <div class="col-md-6  infoficheartiste">
+          <p><strong>QUATRE ARTISTES MUSICIENS</strong></p>
+          <p>Chanteuse</p>
+          <p>Guitare</p>
+          <p>Basse</p>
+          <p>Drums</p><br />
+          <p><strong>ARTISTES SUPPLEMENTAIRES</strong></p>
+          <p>Pianiste, choriste</p>
+          <p>Section de cuivres</p>
+        </div>  
+        <div class="col-md-6  infoficheartiste">
+          <p><strong>RÉFÉRENCES</strong></p>
+          <p>LA CABRO D'OR - BAUX DE PROVENCE</p>
+          <p>HARD ROCK CAFÉ - MARSEILLE</p>
+          <p>RIVIERA ST PONS - GEMENOS</p>
+          <p>CHÂTEAU DE ROQUEFEUILLE - POURRIÈRES</p>
         </div>
+      </div>  
     </div>
-   
-</div>
-</section>
--->
-
-
-
-
-
-
-
-
-
-<!--/////////////////garder code après cette ligne//////////////////////////////////////////-->  
-
- </div>
 </div>
 
  
