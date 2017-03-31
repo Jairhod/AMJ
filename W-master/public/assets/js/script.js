d514164c3694a668ef228a09de6cc19a5bcbc38c
Jonathan <perezjonathancdp@gmail.com>
2017-03-31 15:37:40 +0200

    retouches home page

--- W-master/public/assets/js/script.js
+++ W-master/public/assets/js/script.js
@@ -19,7 +19,6 @@ $(document).ready(function(){
     
 /* API FACEBOOK : pour fil d'actu */
 
-
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -76,43 +75,7 @@ $(document).ready(function(){
 
     });
 
-/* BARRE DERECHERCHE PAGE D'ACCUEIL ====== */
-    
-    /*var s = $('input'),
-    f  = $('form'),
-    a = $('.after'),
-    m = $('h4');
 
-    s.focus(function(){
-      if( f.hasClass('open') ) return;
-      f.addClass('in');
-      setTimeout(function(){
-        f.addClass('open');
-        f.removeClass('in');
-      }, 1300);
-    });
-
-    a.on('click', function(e){
-      e.preventDefault();
-      if( !f.hasClass('open') ) return;
-       s.val('');
-      f.addClass('close');
-      f.removeClass('open');
-      setTimeout(function(){
-        f.removeClass('close');
-      }, 1300);
-    })
-
-    f.submit(function(e){
-      e.preventDefault();
-      m.html('Thanks, high five!').addClass('show');
-      f.addClass('explode');
-      setTimeout(function(){
-        s.val('');
-        f.removeClass('explode');
-        m.removeClass('show');
-      }, 3000);
-    })*/
     
 /* BACK OFFICE : menu ==================== */
     
