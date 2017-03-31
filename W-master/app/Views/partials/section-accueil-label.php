50fc9faaab4fbc23440e49c6c3b8e0f7697975ca
Jonathan <perezjonathancdp@gmail.com>
2017-03-31 14:39:19 +0200

    retouche section label apge d'accueil

--- W-master/app/Views/partials/section-accueil-label.php
+++ W-master/app/Views/partials/section-accueil-label.php
@@ -5,6 +5,7 @@
                        <!-- <div class="label-img"><img src="assets/media/img/studio-1.jpg" alt="photo du label" class="img-responsive"></div>-->
                         <article class="col-label-1">
                             <div class="label-descriptif">
+                                <h6>AMJ prod, qu'est-ce que c'est ?</h6>
                                 <p>Label, agence de booking, organisateur de concerts et même d’un festival d’été, AMJ multiplie les casquettes, démontrant le dynamisme sans faille de la vrégion Provence-Alpes-Cote-D'Azur dans la sphère musicale éclectique. Son leitmotiv : une démarche résolument positive et un esprit fraternel salvateur.</p>
                                 <h6>COMMENT EST NÉ AMJ ?</h6>
                                 <p>En 2000, plusieurs groupes se croisaient régulièrement sur les petites scènes de La région. Ils ont décidé de se fédérer, selon un principe simple : l’union fait la force. A partir de là, ils ont créé un site Internet – Myspace n’existait même pas, à l’époque… Petit à petit, les activités se sont développées. Le collectif a commencé à organiser des concerts dans des petites salles avec des artistes étrangers “indie” et des premières parties assurées par des groupes du collectif. Les choses ont continué à se développer. Depuis neuf ou dix ans, on a une agence de booking et une section label et, depuis cinq ans, notre festival d’été, le Micro Festival.</p>
