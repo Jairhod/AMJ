df9d4fc10056c6fc4602785e6927f3d1a6556261
Jairhod <benjair@gmail.com>
2017-04-01 04:02:45 +0200

    comment reset.min in back header

--- W-master/app/Views/partials/back-header.php
+++ W-master/app/Views/partials/back-header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title><?php echo $this->e($titre); ?></title>
   
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<!--   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"> -->
   <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
   <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
   <link rel="stylesheet" type="text/css" href="../assets/css/style-jo.css">
