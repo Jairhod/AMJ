4fdb02c4d69eaf4f3aedba636bd7bd04683d1dbb
Jonathan <perezjonathancdp@gmail.com>
2017-04-03 22:20:43 +0200

    ajax qui marche pas !

--- W-master/app/Controller/JoController.php
+++ W-master/app/Controller/JoController.php
@@ -6,8 +6,35 @@ use \Model\JoModel;
 
 class JoController extends FormController
 {  
-    public function listeGenresAccueil ()
-    {
-        $nomArtiste             = $this->verifierSaisie("nomArtiste");
-    }
+    
+    public function traiterForm()
+	{
+		// TABLEAU QUI VA CONTENIR LES INFOS A RENVOYER AU NAVIGATEUR
+		$tabReponseJson = [];
+		
+		// DETECTER SI IL Y A UN FORMULAIRE A TRAITER
+		$idForm = $this->verifierSaisie("idForm");
+		if ($idForm == "artisteListeAcceuil")
+		{
+			// IL FAUT TRAITER LE FORMULAIRE DE NEWSLETTER
+			$this->artisteListeAcceuil();
+			
+			// AVANTAGE DE AJAX
+			// JE N'ENVOIE QUE LA REPONSE A LA REQUETE
+			// JE N'ENVOIE PAS LA PAGE HTML ENTIERE
+			$tabReponseJson["retour"] = $GLOBALS["artisteListeAcceuilRetour"];
+		}
+
+		// RENVOYER LE TABLEAU D'INFO JSON
+		// TRANSFORME LE TABLEAU ASSOCIATIF PHP
+		// EN OBJET JAVASCRIPT
+		// (JSON => JS Object Notation)
+		// LA METHODE EFFECTUE json_encode
+		$this->showJson($tabReponseJson);
+		
+		// CREE LA PAGE HTML
+		$this->show('accueil');
+	}   
+
+}	
 
