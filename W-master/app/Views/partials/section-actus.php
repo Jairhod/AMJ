2bafb094e3a7a3c6aacffe8edb619ec3a1fe8245
Jonathan <perezjonathancdp@gmail.com>
2017-04-04 17:04:44 +0200

    recup actu sans responsive foireux!

--- W-master/app/Views/partials/section-actus.php
+++ W-master/app/Views/partials/section-actus.php
@@ -4,15 +4,14 @@
         <h2>Restez informés des nouveaux évènements, clips du label AMJ !</h2>
 	</div>
 	<div class="fb-actu">
-      <p>Toutes les dernières actus sont sur notre facebook ! Suivez-nous !<br>Toutes les vidéos et les clips de notre artistes sur notre chaine youtube, suivez-nous !</p>
        <div class="row-actu">
             <div class="col-actu act-1">
-               
-               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAMJ-Productions-867525126641828%2F&tabs=timeline&width=700px&height500px&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=1417053915025497" width="500px" height="700px" style="border:none;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+               <p>Toutes les dernières actus sont sur notre facebook ! Suivez-nous !</p>
+               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAMJ-Productions-867525126641828%2F&tabs=timeline&width=700px&height500px&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=1417053915025497" width="500px" height="500px" style="border:none;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
             </div>
             <div class="col-actu act-2">
-                <p></p>
-                <iframe width="700px" height="500px" src="https://www.youtube.com/embed/DV0XRlJY040?list=UUg6TbHDPivo--VikpRLXrTQ&amp;controls=0" frameborder="0" allowfullscreen></iframe>
+                <p>Toutes les vidéos et les clips de notre artistes sur notre chaine youtube, suivez-nous !</p>
+                <iframe width="700" height="500" src="https://www.youtube.com/embed/DV0XRlJY040?list=UUg6TbHDPivo--VikpRLXrTQ&amp;controls=0" frameborder="0" allowfullscreen></iframe>
 	        </div>
 	   </div><!-- fin div row-actu-->
 	</div> <!--fin div fb-actu-->
