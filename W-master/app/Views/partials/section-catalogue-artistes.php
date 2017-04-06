3ef283d57789ad975731063720c3a53ef9745af2
Jairhod <benjair@gmail.com>
2017-04-06 15:40:29 +0200

    nav catalogue réparé

--- W-master/app/Views/partials/section-catalogue-artistes.php
+++ W-master/app/Views/partials/section-catalogue-artistes.php
@@ -1,35 +1,4 @@
-
-<section id="artistes">
-    <div class="contenu-intro-catalogue">
-        <h1>LE CATALOGUE AMJ PROD</h1>                       
-        <p>Consultez, cherchez et trouvez ici l'artiste, le groupe qui convient pour votre évènement ! </p>
-        <div class="bla">
-            <div id="recherche-artiste">
-                <form action="" method="GET" class="section-accueil">
-                    <input type="hidden" name="idForm" value="choixCatalogue">              
-                        <select name="genres" id="genres" class="form-control form-accueil">
-                            <option value="hide" selected disabled>Choisir son style...</option>         
-                            <option value="tous">TOUS LES STYLES</option>
-                            <?php
-                                $objetArtistesModel = new \Model\ArtistesModel;
-                                $tabLigne = $objetArtistesModel->findAll("nomGenre", "ASC");
-                                $tableGenre = []; 
-                                foreach($tabLigne as $index => $tabColonne)
-                                {
-                                    $id                  = $tabColonne["id"];
-                                    $nomGenre            = $tabColonne["nomGenre"];
-                                    $tabGenre[$nomGenre] = $nomGenre;
-                                }
-                                foreach($tabGenre as $nomColonne => $valeurColonne)
-                                {
-                                    echo "<option classe='genre'>$valeurColonne</option>";        
-                                }                                   
-                            ?>      
-                        </select>
-                </form>
-            </div>
-        </div>  
-    </div>   
+ 
         <div class="grid artistes-row">            
                         <?php
                             $objetArtistesModel = new \Model\ArtistesModel;
